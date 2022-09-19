b6fd4ef9a3bd8f17a9c198bf6d725615018668f4
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-09-19 18:31:43 +0000

    ActionView::Template::Error (undefined method `id' for nil:NilClass):

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'tweets#new'
   post 'tweets', to: 'tweets#create'
   root 'tweets#create'
+  delete 'tweets/:id', to: 'tweets#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
