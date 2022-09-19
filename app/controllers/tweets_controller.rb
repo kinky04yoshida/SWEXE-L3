65241e069bfbe8e38b270c6b2b543700c8de7b35
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-09-19 19:13:25 +0000

    Completed 200 OK in 78ms (Views: 73.6ms | ActiveRecord: 0.8ms)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,17 +3,25 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+  
   def new
     @tweet = Tweet.new
   end
+  
   def create
     tweet = Tweet.new(message: params[:tweet][:message], tdate: Time.current)
     tweet.save
     redirect_to '/'
   end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy
     redirect_to '/'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'tweets#new'
   post 'tweets', to: 'tweets#create'
   root 'tweets#create'
+  get 'tweets/:id', to: 'tweets#show'
   delete 'tweets/:id', to: 'tweets#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
